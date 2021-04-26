1673846cfe88f6a8f25b347c0599d554f651db3d
WenjieLiuu <56020612@qq.com>
2021-04-26 17:35:38 +0800

    make gltf loader support to load points(glitter)

--- examples/jsm/loaders/GLTFLoader.js
+++ examples/jsm/loaders/GLTFLoader.js
@@ -7,6 +7,7 @@
  */
 
 import {
+	AdditiveBlending,
 	AnimationClip,
 	Bone,
 	Box3,
@@ -56,6 +57,7 @@ import {
 	RGBAFormat,
 	RGBFormat,
 	RepeatWrapping,
+	ShaderMaterial,
 	Skeleton,
 	SkinnedMesh,
 	Sphere,
@@ -232,6 +234,10 @@ var GLTFLoader = ( function () {
 							extensions[ extensionName ] = new GLTFHighPrecisionNormalExtension();
 							break;
 
+						case EXTENSIONS.OFT_MATERIALS_POINT_SPRITE:
+							extensions[ extensionName ] = new GLTFMaterialsPointSpriteExtension();
+							break;
+
 						case EXTENSIONS.KHR_LIGHTS_PUNCTUAL:
 							extensions[ extensionName ] = new GLTFLightsExtension( json );
 							break;
@@ -334,6 +340,7 @@ var GLTFLoader = ( function () {
 
 	var EXTENSIONS = {
 		OFT_TEXTURE_HIGHPRECISION_NORMAL: 'OFT_texture_highPrecisionNormal',
+		OFT_MATERIALS_POINT_SPRITE: 'OFT_materials_pointSprite',
 		KHR_BINARY_GLTF: 'KHR_binary_glTF',
 		KHR_DRACO_MESH_COMPRESSION: 'KHR_draco_mesh_compression',
 		KHR_LIGHTS_PUNCTUAL: 'KHR_lights_punctual',
@@ -373,6 +380,188 @@ var GLTFLoader = ( function () {
 
 		this.name = EXTENSIONS.OFT_TEXTURE_HIGHPRECISION_NORMAL;
 
+	}
+
+	/**
+	 * Point Sprite Materials Extension
+	 *
+	 * Specification: https://github.com/oppenfuture/glTF/tree/pointSprite/extensions/2.0/Vendor/OFT_materials_pointSprite
+	 */
+
+	 function OFTPointSprintMaterial( params ) {
+
+		ShaderMaterial.call( this, params );
+		this.type = 'OFTPointSprintMaterial';
+
+	 }
+
+	 OFTPointSprintMaterial.prototype = Object.create( ShaderMaterial.prototype );
+	 OFTPointSprintMaterial.prototype.constructor = OFTPointSprintMaterial;
+
+	 OFTPointSprintMaterial.prototype.copy = function ( source ) {
+
+		 ShaderMaterial.prototype.copy.call( this, source );
+		 return this;
+
+	 };
+
+	 function GLTFMaterialsPointSpriteExtension() {
+
+		this.name = EXTENSIONS.OFT_MATERIALS_POINT_SPRITE;
+
+	}
+
+	GLTFMaterialsPointSpriteExtension.prototype.getMaterialType = function () {
+
+		return OFTPointSprintMaterial;
+
+	};
+
+	GLTFMaterialsPointSpriteExtension.prototype.extendParams = function ( materialParams, materialDef, parser ) {
+		var pending = [];
+
+		var pointSprite = materialDef.extensions?.OFT_materials_pointSprite;
+
+		if ( pointSprite ) {
+
+			materialParams.pointSize = pointSprite.pointSize
+
+			if ( pointSprite.pointTexture !== undefined ) {
+
+				pending.push( parser.assignTexture( materialParams, 'pointTexture', pointSprite.pointTexture ) );
+
+			}
+
+		}
+
+		return Promise.all( pending );
+
+	};
+
+	GLTFMaterialsPointSpriteExtension.prototype.createMaterial = function ( materialParams ) {
+		var uniformsPoint = {
+			pointSize: { value: materialParams.pointSize * window.devicePixelRatio },
+			pointTexture: { value: materialParams.pointTexture },
+			depthTexture: { value: null },
+			depthBias: { value: 0.01 }
+		};
+		var material = new OFTPointSprintMaterial( {
+			uniforms: uniformsPoint,
+			vertexShader: /* glsl */`
+			attribute float size;
+
+			uniform float pointSize;
+
+			varying vec4 vColor;
+			varying float vIntensity;
+			varying float vRandom;
+			varying vec4 vMvpPosition;
+
+			#define PI 3.14159265359
+			#define PI2 6.28318530718
+
+			const float Threshold = 0.65;
+
+			highp float rand( const in vec2 uv ) {
+				const highp float a = 12.9898, b = 78.233, c = 43758.5453;
+				highp float dt = dot( uv.xy, vec2( a, b ) ), sn = mod( dt, PI );
+				return fract(sin(sn) * c);
+			}
+
+			void main() {
+
+				vColor.rgb = color;
+
+				vRandom = rand(position.xy);
+				vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );
+				vec3 ViewPosition = - mvPosition.xyz;
+				vec3 viewDir = normalize(ViewPosition);
+
+				vec3 n = normalize(normalMatrix * normal);
+				float dotNV = dot(viewDir, n);
+				// cos func to liner func
+				vIntensity = 1.0 - acos(dotNV);
+				// map Threshold ~ 1.0 to 0 ~ 1.0
+				vIntensity = vIntensity < Threshold ? 0.0 : (vIntensity - Threshold) / (1.0 - Threshold);
+				// sharp the curve
+				vIntensity = pow(vIntensity, 1.1);
+
+				vColor.a = 0.2 + vIntensity * 0.8;
+
+				gl_Position = projectionMatrix * mvPosition;
+
+				vMvpPosition = gl_Position;
+				gl_PointSize = ((vRandom * 0.2 + 1.8) * pointSize) * vIntensity * (0.5/-mvPosition.z);
+
+			}
+			`,
+			fragmentShader: /* glsl */`
+			precision highp float;
+			#include <packing>
+
+			#define PI 3.14159265359
+			#define PI2 6.28318530718
+
+			uniform sampler2D pointTexture;
+			uniform sampler2D depthTexture;
+			uniform sampler2D colorTexture;
+			uniform float depthBias;
+
+			varying vec4 vColor;
+			varying float vRandom;
+			varying float vDotNV;
+			varying float vIntensity;
+			varying vec4 vMvpPosition;
+
+			vec2 rotateUV(vec2 uv, float rotation) {
+				float mid = 0.5;
+				return vec2(
+					cos(rotation) * (uv.x - mid) + sin(rotation) * (uv.y - mid) + mid,
+					cos(rotation) * (uv.y - mid) - sin(rotation) * (uv.x - mid) + mid
+				);
+			}
+
+			vec3 rgb2hsv(vec3 c) {
+				vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+				vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+				vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+				float d = q.x - min(q.w, q.y);
+				float e = 1.0e-10;
+				return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+			}
+
+			vec3 hsv2rgb(vec3 c) {
+				vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+				vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+				return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+			}
+
+			float readDepth( sampler2D depthSampler, vec2 coord ) {
+				float fragCoordZ = texture2D( depthSampler, coord ).x;
+				float viewZ = perspectiveDepthToViewZ( fragCoordZ, 0.1, 2.0 );
+				return viewZToOrthographicDepth( viewZ, 0.1, 2.0 );
+			}
+
+			void main() {
+				vec3 projCoords = vMvpPosition.xyz / vMvpPosition.w;
+				projCoords = projCoords * 0.5 + 0.5;
+				float depth = unpackRGBAToDepth(texture2D(colorTexture, projCoords.xy));
+				if (projCoords.z - depth >= 0.001) discard;
+
+				if (vIntensity <= 0.0) discard;
+				gl_FragColor = vColor;
+				vec2 rotateUv = rotateUV(gl_PointCoord, vRandom * vIntensity * PI * 0.6);
+				gl_FragColor = gl_FragColor * texture2D( pointTexture, rotateUv );
+			}
+			`,
+
+			blending: AdditiveBlending,
+			depthTest: false,
+			transparent: true,
+			vertexColors: true
+		} );
+		return material;
 	}
 
 	/**
@@ -2077,7 +2266,12 @@ var GLTFLoader = ( function () {
 		var useMorphTargets = Object.keys( geometry.morphAttributes ).length > 0;
 		var useMorphNormals = useMorphTargets && geometry.morphAttributes.normal !== undefined;
 
-		if ( mesh.isPoints ) {
+		if ( material.type === 'OFTPointSprintMaterial' ) {
+
+			// TODO: Why need this line?
+			mesh = mesh.clone();
+
+		} else if ( mesh.isPoints ) {
 
 			var cacheKey = 'PointsMaterial:' + material.uuid;
 
@@ -2203,6 +2397,12 @@ var GLTFLoader = ( function () {
 		} else if ( materialExtensions[ EXTENSIONS.KHR_MATERIALS_UNLIT ] ) {
 
 			var kmuExtension = extensions[ EXTENSIONS.KHR_MATERIALS_UNLIT ];
+			materialType = kmuExtension.getMaterialType();
+			pending.push( kmuExtension.extendParams( materialParams, materialDef, parser ) );
+
+		} else if ( materialExtensions[ EXTENSIONS.OFT_MATERIALS_POINT_SPRITE ] ) {
+
+			var kmuExtension = extensions[ EXTENSIONS.OFT_MATERIALS_POINT_SPRITE ];
 			materialType = kmuExtension.getMaterialType();
 			pending.push( kmuExtension.extendParams( materialParams, materialDef, parser ) );
 
@@ -2349,6 +2549,10 @@ var GLTFLoader = ( function () {
 			if ( materialType === GLTFMeshStandardSGMaterial ) {
 
 				material = extensions[ EXTENSIONS.KHR_MATERIALS_PBR_SPECULAR_GLOSSINESS ].createMaterial( materialParams );
+
+			}else if ( materialType === OFTPointSprintMaterial ) {
+
+				material = extensions[ EXTENSIONS.OFT_MATERIALS_POINT_SPRITE ].createMaterial( materialParams );
 
 			} else {
 

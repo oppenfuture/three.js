1954a77e088994246acc862cb31e80cdfc2979ce
JichenZhang <JichenZhang@users.noreply.github.com>
2025-04-02 07:24:28 +0000

    Release 0.137.17

--- build/three.cjs
+++ build/three.cjs
@@ -30958,15 +30958,15 @@ class PointLightHelper extends Mesh {
 		// TODO: delete this comment?
 		const distanceGeometry = new THREE.IcosahedronBufferGeometry( 1, 2 );
 		const distanceMaterial = new THREE.MeshBasicMaterial( { color: hexColor, fog: false, wireframe: true, opacity: 0.1, transparent: true } );
-			this.lightSphere = new THREE.Mesh( bulbGeometry, bulbMaterial );
+		this.lightSphere = new THREE.Mesh( bulbGeometry, bulbMaterial );
 		this.lightDistance = new THREE.Mesh( distanceGeometry, distanceMaterial );
-			const d = light.distance;
-			if ( d === 0.0 ) {
-				this.lightDistance.visible = false;
-			} else {
-				this.lightDistance.scale.set( d, d, d );
-			}
-			this.add( this.lightDistance );
+		const d = light.distance;
+		if ( d === 0.0 ) {
+			this.lightDistance.visible = false;
+		} else {
+			this.lightDistance.scale.set( d, d, d );
+		}
+		this.add( this.lightDistance );
 		*/
 	}
 	dispose() {
@@ -30982,12 +30982,12 @@ class PointLightHelper extends Mesh {
 
 		/*
 		const d = this.light.distance;
-				if ( d === 0.0 ) {
-					this.lightDistance.visible = false;
-				} else {
-					this.lightDistance.visible = true;
+			if ( d === 0.0 ) {
+				this.lightDistance.visible = false;
+			} else {
+				this.lightDistance.visible = true;
 			this.lightDistance.scale.set( d, d, d );
-				}
+			}
 		*/
 	}
 }
@@ -31372,7 +31372,7 @@ class BoxHelper extends LineSegments {
 		1/___0/|
 		| 6__|_7
 		2/___3/
-				0: max.x, max.y, max.z
+			0: max.x, max.y, max.z
 		1: min.x, max.y, max.z
 		2: min.x, min.y, max.z
 		3: max.x, min.y, max.z
@@ -31629,7 +31629,7 @@ class SkinnedMeshBoxHelper extends LineSegments {
 		1/___0/|
 		| 6__|_7
 		2/___3/
-				0: max.x, max.y, max.z
+			0: max.x, max.y, max.z
 		1: min.x, max.y, max.z
 		2: min.x, min.y, max.z
 		3: max.x, min.y, max.z
